1601d396917973efb7bf9a418c9392e268f04f5c
khuram <knshaikh@gmail.comgit config --global user.email knshaikh@gmail.comgit config --global user.email knshaikh@gmail.com>
2017-05-10 19:19:07 +0000

    downloadImage function added through request module

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,42 @@
 var request = require('request');
+var fs = require('fs');
+
 var GITHUBUSER = "Gsync";
 var GITHUBTOKEN = "5f04057bd82a2c85e456d988fa5f1222fdcd80bf";
 console.log("welcome to the github avatar downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
-              // Note 4
+
 var requestURL = 'https://' + GITHUBUSER + ':' + GITHUBTOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 console.log(requestURL);
+
+request.get(requestURL)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+        //response.setHeader("User-Agent", "Gsync");
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('body:', response);
+       })
+
+}
+
+function downloadImageByURL(url, filePath) {
+  var requestURL = url + "/" + filePath;
+
+  request.get(requestURL)
+          .on('error', function(error) {
+            throw err;
+          })
+          .on('reponse', function(reposne) {
+            console.log('Response Status Code', response.statusMessage);
+            console.log('Downloading Image...');
+            console.log('Download Complete!');
+            console.log(response.headers['content-type']);
+          })
+          .pipe(fs.createWriteStream('./downloadedImage.jpg'))
+  console.log(requestURL);
 
 }
 
@@ -14,3 +44,5 @@ getRepoContributors("jquery", "jquery", function() {
   console.log("Errors:", err);
   console.log("Errors:", result);
 });
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
